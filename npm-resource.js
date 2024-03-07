3d6e3ecf575e92cecd844d2772d47e14c3caad55
abrahamlop <abrahamofez@gmail.com>
2024-03-07 13:42:18 -0600

    version 1.1.0

--- npm-resource.js
+++ npm-resource.js
@@ -1,7 +1,8 @@
 export class Loader {
   constructor(select, className) {
     this.$loader = document.createElement("img");
-    this.$loader.src = "https://media.tenor.com/On7kvXhzml4AAAAj/loading-gif.gif";
+    this.$loader.src =
+      "https://media.tenor.com/On7kvXhzml4AAAAj/loading-gif.gif";
     this.$loader.alt = "Loading";
     this.$loader.classList.add(className);
     document.querySelector(select).appendChild(this.$loader);
@@ -12,7 +13,7 @@ export class Loader {
       this.$loader.parentNode.removeChild(this.$loader);
     } else {
       const loader = document.querySelector(".loader");
-        loader.remove();
+      loader.remove();
     }
   }
 }
@@ -22,20 +23,87 @@ export function Observer(selector, addClassName) {
   const observedSections = document.querySelectorAll(selector);
 
   function handleIntersection(entries, observer) {
-    entries.forEach(entry => {
+    entries.forEach((entry) => {
       if (entry.isIntersecting) {
         entry.target.classList.add(addClassName);
-      }
-      else {
+      } else {
         entry.target.classList.remove(addClassName);
       }
-
     });
   }
 
   const observer = new IntersectionObserver(handleIntersection);
 
-  observedSections.forEach(section => {
+  observedSections.forEach((section) => {
     observer.observe(section);
+  });
+}
+
+export function NavBarMenu(select, callback, ...rest) {
+  const width = rest[0] || "40px";
+  const height = rest[1] || "50px";
+  const color = rest[2] || "black";
+  const height_bar = rest[3] || "5px";
+
+  const hamMenu = document.createElement("div");
+  const $container = document.querySelector(select);
+  hamMenu.innerHTML = `
+      <span></span>
+      <span></span>
+      <span></span>`;
+  hamMenu.classList.add("ham-menu");
+  $container.appendChild(hamMenu);
+
+  // Add style
+  const style = document.createElement("style");
+  style.innerHTML = `
+      /* ham menu */
+      .ham-menu {
+          height: ${height};
+          width: ${width};
+          margin-left: auto;
+          position: relative;
+          cursor: pointer;
+      }
+      .ham-menu span {
+          height: ${height_bar};
+          width: 100%;
+          background-color: ${color};
+          border-radius: 25px;
+          position: absolute;
+          left: 50%;
+          top: 50%;
+          transform: translate(-50%, -50%);
+          transition: .3s ease;
+      }
+      .ham-menu span:nth-child(1) {
+          top: 25%;
+      }
+      .ham-menu span:nth-child(3) {
+          top: 75%;
+      }
+      .ham-menu.active span {
+          background-color: ${color};
+      }
+      .ham-menu.active span:nth-child(1) {
+          top: 50%;
+          transform: translate(-50%, -50%) rotate(45deg);
+      }
+      .ham-menu.active span:nth-child(2) {
+          opacity: 0;
+      }
+      .ham-menu.active span:nth-child(3) {
+          top: 50%;
+          transform: translate(-50%, -50%) rotate(-45deg);
+      }
+  `;
+  document.head.appendChild(style);
+
+  // Add event listener
+  hamMenu.addEventListener("click", () => {
+    hamMenu.classList.toggle("active");
+    if (callback && typeof callback === "function") {
+      callback();
+    }
   });
 }
